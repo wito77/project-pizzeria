3556bcf0180cecc19ba7efccb7d6be4b9a3337dc
Kacper Witosław <wito77pl@gmail.com>
2020-02-04 21:54:32 +0100

    Manage pages order and booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import { Product } from './components/Product.js';
 import { Cart } from './components/Cart.js';
-import { select, settings } from './settings.js';
+import { select, settings, classNames } from './settings.js';
 
 const app = {
   initMenu: function () {
@@ -47,6 +47,49 @@ const app = {
     });
   },
 
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+
+    let pagesMatchingHash = [];
+    if (window.location.hash.lenght > 2) {
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      pagesMatchingHash = thisApp.pages.filter(function (page) {
+        return page.id == idFromHash;
+      });
+      thisApp.activatePage(pagesMatchingHash.length ? pagesMatchingHash[0].id : thisApp.pages[0].id);
+    }
+
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* TODO: get page id from href*/
+        const pageId = clickedElement.getAttribute('href');
+        const href = pageId.replace('#', '');
+        /* TODO activate page*/
+        thisApp.activatePage(href);
+      });
+    }
+
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.nav.active, page.getAttribute('id') == pageId);
+    }
+    window.location.hash = '#/' + pageId;
+  },
+
   init: function () {
     const thisApp = this;
     // console.log('*** App starting ***');
@@ -57,6 +100,7 @@ const app = {
 
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initPages();
   },
 };
 
